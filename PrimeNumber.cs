dbf00545cb7d93a03c545945f22e043acf37e687
SzymonKolanowski <75213000+SzymonKolanowski@users.noreply.github.com>
2021-05-01 14:14:44 +0200

    Add files via upload

---
+++ PrimeNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextPrimeNumber
+{
+	public class PrimeNumber
+	{
+		private readonly IOutputWriter outputWriter;
+		private readonly IInputReader inputReader;
+
+		public PrimeNumber(IOutputWriter outputWriter, IInputReader inputReader)
+		{
+			this.outputWriter = outputWriter;
+			this.inputReader = inputReader;
+		}
+
+		public string PlayerNumber { get; private set; }
+
+		public void number()
+		{
+			outputWriter.SendToOutput("podaj jakąś liczbę dodatnią");
+			PlayerNumber = inputReader.GetInput();
+			outputWriter.SendToOutput("Najbliższa liczba pierwsza to " + PlayerNumber);
+		}
+	}
+	public interface IOutputWriter
+	{
+		void SendToOutput(string output);
+	}
+
+	public interface IInputReader
+	{
+		string GetInput();
+	}
+
+	public class ConsoleOutputWriter : IOutputWriter
+	{
+		public void SendToOutput(string output)
+		{
+			Console.WriteLine(output);
+		}
+	}
+
+	public class ConsoleInputReader : IInputReader
+	{
+		public string GetInput()
+		{
+			return Console.ReadLine();
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace NextPrimeNumber
+{
+	class Program
+	{
+		static bool isPrime(int liczba)
+		{
+			if (liczba == 1)
+			{
+				return false;
+			}
+
+			for (int i = 2; i < liczba; i++)
+			{
+				if (liczba % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		static void Main(string[] args)
+		{
+			//program myta się o liczbę i wyświetla najbliższą liczbę pierwszą.
+			//Dla 8 wynik to 7,
+			//Dla 10 wynik to 11,
+			//Dla 9 wynik to 11 powiedzmy że jeśli różnica jest taka sama to pokazujesz większą liczbę
+			//Napisz do tego testy
+			//Program ma działać tak długo, aż podasz komendę stop	
+			//Program powinien wyświetlać wynik na konsoli oraz zapisywać do pliku
+			
+			Console.WriteLine("podaj jakąś liczbę dodatnią");
+			string text = Console.ReadLine();
+			
+			while (text != "stop")
+			{
+				int liczba = int.Parse(text);
+				bool pierwsza = isPrime(liczba);
+				if (pierwsza == false)
+				{
+					liczba++;
+					Console.WriteLine("Najbliższa liczba pierwsza to " + liczba);
+					Console.WriteLine("podaj następną liczbę lub napisz 'stop' jeśli chcesz skończyć");
+					text = Console.ReadLine();
+					
+				}
+				else 
+				{
+					Console.WriteLine("Najbliższa liczba pierwsza to " + liczba);
+					Console.WriteLine("podaj następną liczbę lub napisz 'stop' jeśli chcesz skończyć");
+					text = Console.ReadLine();
+					
+				}
+
+				var consoleOutput = new ConsoleOutputWriter();
+				var consoleInput = new ConsoleInputReader();
+				var primenumber = new PrimeNumber(consoleOutput, consoleInput);
+
+				primenumber.number();
+
+			}
+			
+		}
+	}
+
+}
+	
+
